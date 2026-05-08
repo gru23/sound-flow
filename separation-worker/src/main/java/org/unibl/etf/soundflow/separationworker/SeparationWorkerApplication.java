b4f3c1312ab66adf1d04dea2fba3c79636d20643
gru <grujica.vasic@hotmail.com>
2026-05-08 11:46:16 +0200

    Initial commit with separation-worker and demucs

---
+++ separation-worker/src/main/java/org/unibl/etf/soundflow/separationworker/SeparationWorkerApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.soundflow.separationworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeparationWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeparationWorkerApplication.class, args);
+	}
+
+}
---
+++ separation-worker/src/test/java/org/unibl/etf/soundflow/separationworker/SeparationWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.soundflow.separationworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeparationWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
