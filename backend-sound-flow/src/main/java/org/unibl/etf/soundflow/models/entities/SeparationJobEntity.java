2cbf228d093473af39cc1938c866bd1dfd2fd49f
gru <grujica.vasic@hotmail.com>
2026-06-05 11:27:46 +0200

    added file title in DB

--- backend-sound-flow/src/main/java/org/unibl/etf/soundflow/models/entities/SeparationJobEntity.java
+++ backend-sound-flow/src/main/java/org/unibl/etf/soundflow/models/entities/SeparationJobEntity.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.unibl.etf.soundflow.models.enums.SeparationOption;
 import org.unibl.etf.soundflow.models.enums.SeparationStatus;
 
+import java.io.File;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
@@ -28,6 +29,7 @@ public class SeparationJobEntity {
 
     private String sourcePath;
     private String separatedPath;
+    private String title;
 
     private LocalDateTime createdAt;
     private LocalDateTime startedAt;
@@ -43,6 +45,7 @@ public class SeparationJobEntity {
         this.option = option;
         status = SeparationStatus.QUEUED;
         this.sourcePath = sourcePath;
+        this.title = new File(sourcePath).getName();
         createdAt = LocalDateTime.now();
         this.client = client;
     }
